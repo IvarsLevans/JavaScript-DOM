68d52271a48b6994312cb6c214a4c192bcb724b7
IvarsLevans <ivars.levans@gmail.com>
2021-03-09 16:51:25 +0200

    Tutorial #11

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const list = document.querySelector('#book-list ul');
 
 // delete books
-list.addEventListener('click', (e) => {
+list.addEventListener('click', (e){
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
+    li.removeChild(li);
   }
+});
+
+//add book-list
+const addForm = forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
 });
